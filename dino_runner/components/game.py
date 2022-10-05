40bf46d1354a71cf4548c78514b06fd3a58f8da5
JuanMisaelClaros <Misael.Claros@fundacion-jala.org>
2022-10-05 01:01:41 -0400

    grupo3-mod2 dinosaur, obstacles

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,5 +1,7 @@
 import pygame
 from dino_runner.utils.constants import BG,ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.dinosaur.dinosur import Dinosaur
+from dino_runner.components.obstacles.obstaclemanager import Obstaclemanager
 
 class Game:
     def __init__(self):
@@ -14,6 +16,9 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
+        self.player = Dinosaur()
+        self.obstacle_manager = Obstaclemanager()
+
     def run(self):
         self.playing = True
         while self.playing:
@@ -27,23 +32,28 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+
     def updates(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255,255,255))
         self.draw_background()
+        self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width() - 1100
-        print(image_width)
         self.screen.blit(BG, (self.x_pos_bg,self.y_pos_bg))
         #self.screen.blit(BG, (self.x_pos_bg,self.y_pos_bg))
         if self.x_pos_bg <=-image_width:
             self.screen.blit(BG,(image_width+self.x_pos_bg, self.y_pos_bg)) 
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+        
         
